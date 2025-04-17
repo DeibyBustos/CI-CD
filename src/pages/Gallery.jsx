a7e484ed2a8b2bd57143e3e3347c86b90fa497b5
Deiby Bustos <deibybustos11@gmail.com>
2025-04-17 18:05:42 -0500

    imagen

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -9,7 +9,7 @@ const Gallery = ({ category }) => {
   const galleryData = [
 
     {
-      url: 'https://teoriadelarte.com/wp-content/uploads/2023/11/noche-estrellada-vincent-van-gogh.jpg',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/%22La_noche_estrellada%22_de_Van_gogh.jpg/250px-%22La_noche_estrellada%22_de_Van_gogh.jpg',
       title: 'img1',
       type: 'image',
       category: 'art'
